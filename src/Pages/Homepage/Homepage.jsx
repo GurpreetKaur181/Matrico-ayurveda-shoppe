99466446d130f20065d828b5a3d7c7f61df49d1f
GurpreetKaur181 <gk9000219@gmail.com>
2024-11-16 11:40:22 +0530

    homepage

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -3,21 +3,21 @@ import styles from "./Homepage.module.css";
 
 
 const images = [
-  "ayur.png",
-  "ayur1.png",
-  "ayur2.png",
-  "ayur3.png",
-  "ayur4.png",
-  "ayur5.png",
-  "ayur6.png",
-  "ayur7.png",
-  
+  { name: "Herbal & Ayurveda Diabic Care Juice 1000 ml", src: "ayur.png" },
+  { name: "Pankajakasthuri Breathe Easy Granules 400 gm", src: "ayur1.png" },
+  { name: "Pankajakasthuri Breathe Easy Syrup 200 ml", src: "ayur2.png" },
+  { name: "Lama Ayush kwath Tablet 60's", src: "ayur3.png" },
+  { name: "Lama Draksharist Tonic 450 ml", src: "ayur4.png" },
+  { name: "Kairali Chyawanaprasam Lehya 500 gm", src: "ayur5.png" },
+  { name: "Dabur Honitus Herbal Cough Remedy Sugar Free Syrup 100 ml", src: "ayur6.png" },
+  { name: "Lama Cough Syrup 100 ml", src: "ayur7.png" },
 ];
-function Homepage(){
-   return(
-      <>
-    <div className={styles.container}></div>
-    <div className={styles.textContent}>
+
+function Homepage() {
+  return (
+    <>
+      <div className={styles.container}></div>
+      <div className={styles.textContent}>
         <h1>Welcome to Our Ayurvedic Medicine Collection</h1>
         <p>
           Explore our range of natural and organic Ayurvedic medicines designed
@@ -26,21 +26,25 @@ function Homepage(){
       </div>
 
       <div className={styles.Boxes}>
-        {/* Render 8 boxes with images */}
+        {/* Render 8 boxes with images, names, and buttons */}
         {images.map((image, index) => (
           <div key={index} className={styles.box}>
             <img
-              src={`src/assets/images/${image}`}
-              alt={`Ayurvedic Product ${index + 1}`}
+              src={`src/assets/images/${image.src}`}
+              alt={image.name}
               className={styles.boxImage}
             />
-           
+            <div className={styles.imageDetails}>
+              <p>{image.name}</p> {/* Display the image name */}
+              <button className={styles.button}>View More</button> {/* Add a button */}
+            </div>
           </div>
         ))}
-      </div>  
-      
-        </>
-   )
+      </div>
+    </>
+  );
 }
 
 export default Homepage;
+
+
