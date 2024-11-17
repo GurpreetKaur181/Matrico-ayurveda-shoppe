2b9996ef2cbd315603d205b10e11e90ca5cdad9f
GurpreetKaur181 <gk9000219@gmail.com>
2024-11-17 21:53:36 +0530

    Create Aboutpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Navbar/Nav.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
+import Aboutpage from "./pages/Aboutpage/Aboutpage.jsx";
+import Contactpage from "./pages/Contactpage/Contactpage.jsx";
 import DailyTips from "./pages/Daily_Tips/Daily_Tips.jsx";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<Aboutpage />} />
+        <Route path="/contact" element={<Contactpage />} />
         <Route path="/daily_tips" element={<DailyTips />} />
       </Routes>
     </Router>
